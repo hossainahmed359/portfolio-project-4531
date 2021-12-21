356dbca5a8ee174cc2a565092a3d35034bb07b1a
hossainahmed359 <hossainahmed359@gmail.com>
2021-12-21 15:53:13 +0600

    testimonial section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,7 @@ modalCloses.forEach((modalCloses) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio_container", {
+let swiperPortfolio = new Swiper(".portfolio_container", {
     cssMode: true,
     loop: true,
     navigation: {
@@ -106,7 +106,22 @@ let swiper = new Swiper(".portfolio_container", {
 });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial_container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
 
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerview: 2,
+        }
+    }
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
